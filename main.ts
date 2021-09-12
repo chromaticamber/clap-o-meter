11662c669aa67bd3e7712bb6d697918ab5047013
Amber <86908677+chromaticamber@users.noreply.github.com>
2021-09-12 12:40:34 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
-basic.forever(function () {
-	
+input.onSound(DetectedSound.Loud, function () {
+    Start = input.runningTime()
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
 })
+input.onSound(DetectedSound.Quiet, function () {
+    if (Start > 0) {
+        Time = input.runningTime() - Start
+        Start = 0
+        basic.clearScreen()
+        basic.pause(100)
+        basic.showNumber(Time / 1000)
+    }
+})
+let Time = 0
+let Start = 0
+Start = 0
+input.setSoundThreshold(SoundThreshold.Loud, 150)
+input.setSoundThreshold(SoundThreshold.Loud, 70)
